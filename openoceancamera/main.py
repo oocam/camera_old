78d9e62a4f3bb8376a40e0a1969b2dd611e078d0
utkarsh867 <utkarsh867@gmail.com>
2020-07-30 18:50:22 +0530

    fixes issue with parsing data from pressure sensor

--- openoceancamera/main.py
+++ openoceancamera/main.py
@@ -61,15 +61,17 @@ def readSensorData():
             )
         )
     )
-    (pressureSensor, mstemperatureSensor, depthSensor) = tuple(
-        [
-            float(x)
-            for x in str(
-                subprocess.check_output(
-                    "python ms5837-python/example.py", shell=True, text=True
-                ).split()
-            )
-        ]
+
+    pressureSensorReadings = subprocess.check_output(
+        "python ms5837-python/example.py", shell=True, text=True
+    )
+
+    pressureSensorReadings = pressureSensorReadings.split()
+
+    pressureSensor, mstemperatureSensor, depthSensor = (
+        float(pressureSensorReadings[0]),
+        float(pressureSensorReadings[1]),
+        float(pressureSensorReadings[2]),
     )
 
     return {
