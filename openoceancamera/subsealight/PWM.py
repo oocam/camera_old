4d1ef4d182a02954cbc74d70b947ac8dc56d525b
utkarsh867 <utkarsh867@gmail.com>
2020-07-30 19:46:21 +0530

    fixes setting GPIO mode

--- openoceancamera/subsealight/PWM.py
+++ openoceancamera/subsealight/PWM.py
@@ -12,7 +12,7 @@ pwm = GPIO.PWM(24, 900)  # PIN 12 = Board 32
 def switch_off():
     pwm.ChangeDutyCycle(0)
     pwm.stop()
-    GPIO.cleanup(11)
+    GPIO.cleanup(24)
 
 
 def switch_on(mode):
@@ -27,14 +27,13 @@ def switch_on(mode):
     elif mode == 5:
         freq = 40
 
+    print(freq)
     pwm.ChangeDutyCycle(freq)
 
 
 if __name__ == "__main__":
-    now = time.time()
-    end = now + 5
     x = datetime.datetime.now()
     y = x + datetime.timedelta(0, 5)
-    while datetime.datetime.now() != y:
+    while datetime.datetime.now() < y:
         switch_on(4)
     switch_off()
