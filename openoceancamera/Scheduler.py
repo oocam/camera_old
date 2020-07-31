dd319cb2b8e1b71eae1f91f726f4b4ac7b96ba0b
utkarsh867 <utkarsh867@gmail.com>
2020-07-31 13:30:29 +0530

    Adds code for the time scheduler for WittyPi
    Co-authored-by: slersk <slersk@berkeley.edu>

--- openoceancamera/Scheduler.py
+++ openoceancamera/Scheduler.py
@@ -12,9 +12,13 @@ class Scheduler(object):
 
     def should_start(self):
         for i in range(len(self.schedule_data)):
-            if self.schedule_data[i]["start"] <= self.time_now <= self.schedule_data[i]["stop"]:
+            if (
+                self.schedule_data[i]["start"]
+                <= self.time_now
+                <= self.schedule_data[i]["stop"]
+            ):
                 return i
-        return (-1)
+        return -1
 
     def load_scheduler_data(self, data):
         for i in range(len(data)):
@@ -22,6 +26,36 @@ class Scheduler(object):
                 "start": None,
                 "stop": None,
             }
-            frame["start"] = datetime.strptime(data[i]["start"], '%Y-%m-%d-%H:%M:%S')
-            frame["stop"] = datetime.strptime(data[i]["stop"], '%Y-%m-%d-%H:%M:%S')
+            frame["start"] = datetime.strptime(data[i]["start"], "%Y-%m-%d-%H:%M:%S")
+            frame["stop"] = datetime.strptime(data[i]["stop"], "%Y-%m-%d-%H:%M:%S")
             self.schedule_data.append(frame.copy())
+
+    def time_to_nearest_schedule(self):
+        self.update_current_time()
+        possible_slots = []
+
+        # Get the future slots
+        for slot in self.schedule_data:
+            if slot["start"] >= self.time_now:
+                possible_slots.append(slot)
+
+        # Sorts the slots in case they may not be
+        possible_slots = sorted(possible_slots, key=lambda x: x["start"])
+
+        # Take the difference between the most recent slot's start time and time now
+        delta = possible_slots[0]["start"] - self.time_now
+
+        # Returns an integer value for the time difference in seconds
+        return int(delta.total_seconds())
+
+    def next_future_timeslot(self):
+        self.update_current_time()
+
+        future_slots = []
+        # Get the future slots
+        for slot in self.schedule_data:
+            if slot["start"] >= self.time_now:
+                future_slots.append(slot)
+        # Sorts the slots in case they may not be
+        future_slots = sorted(future_slots, key=lambda x: x["start"])
+        return future_slots[0]
