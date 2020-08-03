6feea5feb8555fb6ff9f2dee0394bdefe8f72a79
utkarsh867 <utkarsh867@gmail.com>
2020-08-03 15:48:18 +0530

    Handles closing the camera recording when closing camera stream

--- openoceancamera/Camera.py
+++ openoceancamera/Camera.py
@@ -1,17 +1,20 @@
 try:
     from picamera import PiCamera
 except:
-    print("Program is not running on a Raspberry Pi. The camera module cannot be loaded")
+    print(
+        "Program is not running on a Raspberry Pi. The camera module cannot be loaded"
+    )
     exit(0)
 
+
 class Camera(object):
-    def __init__(self, resolution=(3200, 2400), shutter_speed = 5000, iso = 0, frequency = 3):
+    def __init__(self, resolution=(3200, 2400), shutter_speed=5000, iso=0, frequency=3):
         self.camera = PiCamera()
         self.set_camera_resolution(resolution)
         self.set_shutter_speed(shutter_speed)
         self.set_iso(iso)
         self.set_capture_frequency(frequency)
-    
+
     def do_capture(self, filename="test.jpg"):
         self.camera.capture(filename)
 
@@ -19,11 +22,13 @@ class Camera(object):
         self.camera.start_recording(filename, format="h264")
 
     def stop_recording(self):
-        self.camera.stop_recording()
-    
+        if self.check_recording():
+            self.camera.stop_recording()
+
     def do_close(self):
+        self.stop_recording()
         self.camera.close()
-    
+
     def retrieve_params(self):
         print("Resolution:", self.camera.resolution)
         print("Shutter speed:", self.camera.shutter_speed)
@@ -31,19 +36,19 @@ class Camera(object):
 
     def set_camera_frame_rate(self, fr):
         self.camera.framerate = fr
-        
+
     def set_camera_resolution(self, resolution):
         self.camera.resolution = resolution
 
     def set_shutter_speed(self, speed=0):
         self.camera.shutter_speed = speed
-        
+
     def set_iso(self, iso):
         self.camera.iso = iso
 
     def set_capture_frequency(self, frequency):
         self.frequency = frequency
-       
+
     def check_recording(self):
         if self.recording:
             return True
