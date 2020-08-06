0f4f3b6e75de13a9781ff2a97cfdf590ceff8c61
Sidhant Gupta <sdhnt@connect.hku.hk>
2020-08-07 01:05:18 +0800

    update

--- openoceancamera/main.py
+++ openoceancamera/main.py
@@ -290,7 +290,7 @@ def returnConfig():
     if request.method == "GET":
         if camera_config != []:
             response = {
-                "local_time": str(datetime.now()),
+                "local_time": datetime.now().strftime("%H:%M:%S %Z"),
                 "local_timezone": str(camera_config[0]["timezone"]),
                 "config": json.dumps(camera_config),
             }
