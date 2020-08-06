9079074a8710990a8565e6710cd54cafdd06871b
slersk <slersk@berkeley.edu>
2020-08-07 01:57:47 +0800

    Add clear schedule, write time, set shutdown, set restart functions

--- wittypi/wittypi.py
+++ wittypi/wittypi.py
@@ -1,26 +1,26 @@
 import os
 
-
 class WittyPi:
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        # path as path to wittypi.sh script
+        self.path = path
 
     def clear_wittypi_schedule(self):
-        # TODO: Implement code for clearing schedule
-        pass
+        # Clear current schedule
+        os.system(f"sudo sh f{self.path} 10 6")
+        
+    def set_wittypi_time(self):
+        # Set RTC time to system
+        os.system(f"sudo sh {self.path} 2")
 
-    def set_wittypi_time(self, wittypi_time):
-        # TODO
-        pass
-
-    def set_system_time(self, system_time):
-        # TODO
-        pass
+    def set_system_time(self):
+        # Set system time to RTC
+        os.system(f"sudo sh {self.path} 1")
 
     def set_shutdown_time(self, shutdown_time):
-        # TODO: Implement code for setting shutdown time
-        pass
+        # Set Raspberry Pi next shutdown time
+        os.system(f"sudo sh {self.path} 4 {self.shutdown_time}")
 
     def set_startup_time(self, startup_time):
-        # TODO: Implement code for setting startup time
-        pass
+        # Setting Raspberry Pi next startup time
+        os.system(f"sudo sh {self.path} 5 {self.startup_time}")
